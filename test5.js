5c7ac7971ec32a020e095797f598aafec582cf4b
YeonWooKim <ehdqkd616@cau.ac.kr>
2022-03-10 18:14:24 +0900

    z

---
+++ test5.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+// let _path = path.join("C:", "Users", "PC-064", "Downloads");
+// let _path = path.join("C:/Users/PC-064/Downloads");
+
+let _path = path.join("/home/ec2-user");
+
+console.log(_path);
