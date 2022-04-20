6acfc079b8a307209f8d8c297b21ddb574d726c7
YeonWooKim <ehdqkd616@cau.ac.kr>
2022-04-20 16:05:33 +0900

    crypto

---
+++ test6.js
@@ -0,0 +1,7 @@
+﻿var a = 1
+
+console.log("%d %d %d", a++, a++, a++);
+
+var a = 1
+
+console.log("%d %d %d", ++a, ++a, ++a);
---
+++ test7.js
@@ -0,0 +1,2 @@
+var a = '00000000000010000000';
+console.log(a.substring(12, 13));
